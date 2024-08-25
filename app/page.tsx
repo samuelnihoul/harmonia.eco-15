8c8da66c3c2b05003c0eb026435118b17025cc1b
samuelnihoul <samuelnihoul@ecomail.bzh>
2024-08-25 12:26:59 +0200

    rm the last card

--- app/page.tsx
+++ app/page.tsx
@@ -68,19 +68,7 @@ export default function Home() {
           <p>Opinionated time and carbon pricing helper for daily decision-making</p>
         </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+
       </div>
     </main>
   );
